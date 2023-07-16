141b9da26a9ea42761206de889b59198996dfc14
Calvin Lee <Anthony2006Lee@hotmail.com>
2023-07-16 16:52:04 -0500

    Learning how to add an accordion to my page.

--- src/App.js
+++ src/App.js
@@ -1,49 +1,27 @@
-import { GoBell, GoCloud, GoDatabase } from "react-icons/go";
-import Button from "./Button";
+import Accordion from "./components/Accordion";
 
 function App() {
-  const handleClick = () => {
-    console.log("click!!");
-  };
-
-  return (
-    <>
-      <div>
-        <Button
-          secondary
-          outline
-          rounded
-          className="mb-5"
-          onClick={handleClick}
-        >
-          <GoBell />
-          Click Me!
-        </Button>
-      </div>
-      <div>
-        <Button danger outline>
-          <GoCloud />
-          Buy Now
-        </Button>
-      </div>
-      <div>
-        <Button warning>
-          <GoDatabase />
-          See Deal!
-        </Button>
-      </div>
-      <div>
-        <Button secondary outline>
-          Hide Ads!
-        </Button>
-      </div>
-      <div>
-        <Button primary rounded>
-          Something!
-        </Button>
-      </div>
-    </>
-  );
+  const items = [
+    {
+      id: "hello1",
+      label: "Can I reuse React on a project",
+      content:
+        "You can use React on any project you want.You can use React on any project you want.You can use React on any project you want.",
+    },
+    {
+      id: "hello2",
+      label: "Can I reuse JavaScript on a project",
+      content:
+        "You can use React on any project you want.You can use React on any project you want.You can use React on any project you want.",
+    },
+    {
+      id: "hello3",
+      label: "Can I reuse CSS on a project",
+      content:
+        "You can use React on any project you want.You can use React on any project you want.You can use React on any project you want.",
+    },
+  ];
+  return <Accordion items={items} />;
 }
 
 export default App;
